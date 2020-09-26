4ba411c5dc2991d16aa9af601c4ac720a9c9dacc
SektorDV <roman@sawblade.pl>
2020-09-27 01:58:34 +0200

    changed server addresses

--- src/config.js
+++ src/config.js
@@ -1,2 +1,5 @@
-export const MACHINE_LIST_URL = "https://unfold.azurewebsites.net/agents";
-export const SIGNALR_URL = "https://unfold.azurewebsites.net/gadget";
+// export const MACHINE_LIST_URL = "https://unfold.azurewebsites.net/agents";
+export const MACHINE_LIST_URL =
+  "https://webscoket.noinputsignal.com/gadget/agents";
+// export const SIGNALR_URL = "https://unfold.azurewebsites.net/gadget";
+export const SIGNALR_URL = "https://webscoket.noinputsignal.com/gadget/gadget/";
